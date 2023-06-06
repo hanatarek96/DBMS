141dfec994f452044e7a43681952ce98061f796c
janaelgohary <135715316+janaelgohary@users.noreply.github.com>
2023-06-06 18:44:56 +0300

    Update dbms.sh

--- dbms.sh
+++ dbms.sh
@@ -127,23 +127,10 @@ esac
 }
 
 function selectAll {
-  echo -e "Enter Table Name: \c"
-  read tName
-  column -t -s '|' $tName 2>>./.error.log
-  if [[ $? != 0 ]]
-  then
-    echo "Error Displaying Table $tName"
-  fi
-  selectMenu
-}
-
-
-
-function selectAll {
  echo -e "Enter Table Name:  \c"
  read tName
  column -t -s '|' $tName 2>>./.error.log
- if [[ $? != 0 ]]
+ if [[ $? != 0 ]];
  then
    echo "Error Displaying Table $tName"
  fi
